c36c3767a096ea874dd6de19e91600d1ea6e659b
James Munns <james.munns@ferrous-systems.com>
2019-12-14 18:50:17 +0100

    Fill in example, no main yet

--- examples/from-scratch/src/main.rs
+++ examples/from-scratch/src/main.rs
@@ -1,14 +1,50 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
+use core::{
+    panic::PanicInfo,
+    ptr::{read, write_volatile},
+};
 
 #[link_section = ".vector_table.reset_vector"]
 #[no_mangle]
-pub static __RESET_VECTOR: unsafe extern "C" fn() -> ! = Reset;
+pub static __RESET_VECTOR: fn() -> ! = reset_handler;
 
-#[no_mangle]
-pub unsafe extern "C" fn Reset() -> ! {
+pub fn reset_handler() -> ! {
+    extern "C" {
+        // These symbols come from `linker.ld`
+        static mut __sbss: u32; // Start of .bss section
+        static mut __ebss: u32; // End of .bss section
+        static mut __sdata: u32; // Start of .data section
+        static mut __edata: u32; // End of .data section
+        static __sidata: u32; // Start of .rodata section
+    }
+
+    // Initialize (Zero) BSS
+    unsafe {
+        let mut sbss: *mut u32 = &mut __sbss;
+        let ebss: *mut u32 = &mut __ebss;
+
+        while sbss < ebss {
+            write_volatile(sbss, core::mem::zeroed());
+            sbss = sbss.offset(1);
+        }
+    }
+
+    // Initialize Data
+    unsafe {
+        let mut sdata: *mut u32 = &mut __sdata;
+        let edata: *mut u32 = &mut __edata;
+        let mut sidata: *const u32 = &__sidata;
+
+        while sdata < edata {
+            write_volatile(sdata, read(sidata));
+            sdata = sdata.offset(1);
+            sidata = sidata.offset(1);
+        }
+    }
+
+    // Call user main
     main()
 }
 
@@ -18,9 +54,11 @@ fn main() -> ! {
     }
 }
 
-
 /// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+    // On a panic, loop forever
+    loop {
+        continue;
+    }
 }
