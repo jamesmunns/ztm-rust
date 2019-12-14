1f142b390efd84424704d47a0fbcbc743b411170
James Munns <james.munns@ferrous-systems.com>
2019-12-14 18:04:16 +0100

    Builds, but noop

--- examples/from-scratch/src/main.rs
+++ examples/from-scratch/src/main.rs
@@ -1,3 +1,17 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        continue;
+    }
+}
+
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
